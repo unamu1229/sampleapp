43a1dddff7d9c1907f5607b4281a4e30e138c5f3
米田宏 <yonedahiroshi1229@gmail.com>
2019-07-09 22:39:42 +0900

    cs-fixer テスト

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,8 +8,8 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function index()
-    {
+    public function index(){
+        
         $test = "save = 'aaa'; var_dump("lllll");  \$test2->lll";
         $test2 = new \stdClass();
         $test2->$test = 'ss';
