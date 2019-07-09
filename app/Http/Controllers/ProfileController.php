516f99bc8cd3f886014c8fc0442068fe87db1936
米田宏 <yonedahiroshi1229@gmail.com>
2019-07-09 23:33:56 +0900

    pre-hook

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,31 +8,28 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function index(){
-
-
-        
-        $test = "save = 'aaa'; var_dump("lllll");  \$test2->lll";
+    public function index()
+    {
+        //$test = "save = 'aaa'; var_dump("lllll");  \$test2->lll";
         $test2 = new \stdClass();
-        $test2->$test = 'ss';
+        //$test2->$test = 'ss';
 
         var_dump($test2);
 
         $user = User::find(1);
         if (old()) {
-            $user->licenses = collect(old('license'))->map(function($val){
+            $user->licenses = collect(old('license'))->map(function ($val) {
                 $value = new \stdClass();
                 $value->name = $val;
                 return $value;
             });
-
-
         }
+
         debug($user->licenses);
         return view('profile', ['user' => $user]);
     }
 
-    public function edit(ProfileRequest $request) {
-
+    public function edit()
+    {
     }
 }
