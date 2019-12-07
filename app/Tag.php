5e0d33643b69f96e8ab0ac44a59f2e955c6adcbe
米田宏 <yonedahiroshi1229@gmail.com>
2019-12-08 00:26:03 +0900

    has one through と has many through のリレーション

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    public function userSalary()
+    {
+        return $this->hasOneThrough(
+            UserSalary::class,
+            User::class
+        );
+    }
+
+    public function userLicenses()
+    {
+        return $this->hasManyThrough(UserLicense::class, User::class);
+    }
 }
--- tests/Unit/RerationTest.php
+++ tests/Unit/RerationTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\License;
+use App\Tag;
 use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -15,5 +17,25 @@ class RerationTest extends TestCase
         foreach ($user->simpleLicenses as $license) {
             $this->assertTrue(in_array($license->pivot->type, ['active', 'wish', 'pending']));
         }
+    }
+
+    public function test_reverse()
+    {
+        $license = License::query()->find(1);
+        foreach ($license->users as $user) {
+            $this->assertTrue(is_numeric($user->id));
+        }
+    }
+
+    public function test_has_one_through()
+    {
+        $tag = Tag::query()->find(1);
+        $this->assertEquals(7000, $tag->userSalary->salary);
+    }
+
+    public function test_has_many_through()
+    {
+        $tag = Tag::query()->find(1);
+        print_r($tag->userLicenses->pluck('license_id'));
     }
 }
